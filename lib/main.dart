a718be217ab27febbc685eb205ee4743c503f3cc
buicongphuc <buicongphuc191117@gmail.com>
2022-11-30 17:28:27 +0700

    khoi tao du an

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+// import 'package:myshop/ui/products/product_detail_screen.dart';
+// import 'package:myshop/ui/products/product_overview_screen.dart';
+// import 'package:myshop/ui/products/products_manager.dart';
+// import 'ui/products/user_products_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key: key);
+  
+  @override
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.purple,
+        ).copyWith(
+          secondary: Colors.deepOrange,
+        ),
+      ),
+      home: Container(
+        color: Colors.green,
+      )
+      );
+    }
+}
