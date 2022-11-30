4919152775aaa41432310de9727ffcc1f9ce93fa
buicongphuc <buicongphuc191117@gmail.com>
2022-11-30 18:52:01 +0700

    Xay dung trang cac dat hang

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:myshop/ui/products/product_detail_screen.dart';
 import 'package:myshop/ui/products/product_overview_screen.dart';
 import 'package:myshop/ui/products/products_manager.dart';
 import 'ui/products/user_products_screen.dart';
+import 'ui/orders/orders_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const SafeArea(
-        child: CartScreen()
+        child: OrdersScreen()
       )
       );
     }
